1851e697e9811ae108f8e5014758ee3d1e58c05c
Essenni <essenni.saliha@gmail.com>
2018-07-18 11:46:56 +0000

    Add existing file

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '--(()0b^fulfc#sqif-6c542nmyp-%wbw@p1*oj%oezk%2$=a&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.99.100']
 
 
 # Application definition
